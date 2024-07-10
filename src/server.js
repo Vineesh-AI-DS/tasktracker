11ccec9e9819f4588774aeb6e196060cfb19cabb
Vineesh-AI-DS <vineeshxd3@gmail.com>
2024-07-10 13:59:39 +0530

    first commit

---
+++ src/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const taskRoutes = require('./routes/taskRoutes');
+
+const app = express();
+const PORT = 3000;
+
+// Middleware
+app.use(bodyParser.json());
+
+// Routes
+app.use('/api/tasks', taskRoutes);
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
